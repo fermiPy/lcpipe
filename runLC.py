bad0814edc4c5e77dee8dceb4effc68e16710e54
Matthew Dunseth Wood <mdwood137@gmail.com>
2017-10-10 09:28:39 -0700

    script improvements

--- runLC.py
+++ runLC.py
@@ -1,24 +1,39 @@
 import sys
 from fermipy import utils
 utils.init_matplotlib_backend()
-from fermipy.gtanalysis import *
-from fermipy.roi_model import *
-from fermipy.config import *
+from fermipy.gtanalysis import GTAnalysis
 from fermipy.utils import *
 import yaml
 import pprint
-from fermipy.logger import *
 import numpy
+import argparse
 
 from fermipy.gtanalysis import GTAnalysis
 
-novaname = sys.argv[1] #'V679Car'
+def main():
+        
+    usage = "usage: %(prog)s [config file]"
+    description = "Run fermipy analysis chain."
+    parser = argparse.ArgumentParser(usage=usage,description=description)
 
-gta = GTAnalysis(sys.argv[2])
+    parser.add_argument('--config', default = 'sample_config.yaml')
+    parser.add_argument('--source', default = None)
 
-gta.setup()
-gta.optimize()
+    args = parser.parse_args()
+    gta = GTAnalysis(args.config, fileio={'usescratch' : False})
 
-loc = gta.localize(novaname, free_radius=1.0, update=True)
+    if args.source is None:
+        src_name = gta.roi.sources[0].name
+    
+    gta.setup()
+    gta.optimize()
 
-lc = gta.lightcurve(novaname, binsz=86400.*28.0, free_radius=3.0, use_scaled_srcmap=True)
+    loc = gta.localize(src_name, free_radius=1.0, update=True, make_plots=True)
+    sed = gta.sed(src_name, free_radius=1.0, make_plots=True)
+    gta.tsmap(make_plots=True)
+    gta.write_roi('fit0')    
+    lc = gta.lightcurve(src_name, binsz=86400.*28.0, free_radius=3.0, use_scaled_srcmap=True,
+                        multithread=False)
+
+if __name__ == "__main__":
+    main()
