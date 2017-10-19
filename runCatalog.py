d473e6e55bdcd008c132872821a35535502ef944
Matthew Dunseth Wood <mdwood137@gmail.com>
2017-10-19 12:30:27 -0700

    Improve speed/efficiency of table stacking method.

--- runCatalog.py
+++ runCatalog.py
@@ -12,41 +12,42 @@ def stack_files(files, outfile, new_cols=None):
 
     tables = []
     for hdu in h:
-        if isinstance(hdu,fits.BinTableHDU):
-            tables += [stack_tables(files,hdu.name,new_cols=new_cols)]
+        if isinstance(hdu, fits.BinTableHDU):
+            tables += [stack_tables(files, hdu.name, new_cols=new_cols)]
 
     hdus = [fits.PrimaryHDU()]
     hdus += [fits.table_to_hdu(t) for t in tables]
     hdulist = fits.HDUList(hdus)
-    hdulist.writeto(outfile,overwrite=True)
-
+    hdulist.writeto(outfile, overwrite=True)
 
 
 def stack_tables(files, hdu=None, new_cols=None):
-    
+
     tables = []
     for f in sorted(files):
-        tables += [Table.read(f,hdu=hdu)]
+        tables += [Table.read(f, hdu=hdu)]
 
+    data = {}
+    for i, t in enumerate(tables):
+        for c in tables[0].colnames:
+            data.setdefault(c, [])
+            data[c] += [t[c].data]
+
+    data = {k: np.stack(v) for k, v in data.items()}
     cols = []
     for c in tables[0].colnames:
-
         col = tables[0][c]
         cols += [Column(name=col.name, unit=col.unit, shape=col.shape,
-                        dtype=col.dtype)]
+                        dtype=col.dtype, data=data[c])]
 
-    tab = Table(cols,meta=tables[0].meta)
-
-    for t in tables:
-        row = [ t[c] for c in tables[0].colnames ]
-        tab.add_row(row)
-
+    tab = Table(cols, meta=tables[0].meta)
     if new_cols is not None:
 
         for col in new_cols:
             tab.add_column(col)
-                    
+
     return tab
+
 
 def getPosition(fitsFile):
     hdulist = fits.open(fitsFile)
@@ -54,16 +55,18 @@ def getPosition(fitsFile):
     hdulist.close()
     return locData['ra'][0], locData['dec'][0], locData['glon'][0], locData['glat'][0]
 
+
 def getLC(CLFile):
     hdulist = fits.open(fitsFile)
     return hdulist['LIGHTCURVE'].data
 
+
 def getSourcesFromYaml(yamlName, LCpath):
     with open(yamlName, 'r') as stream:
-       try:
-          yFile = yaml.load(stream)
-       except yaml.YAMLError as exc:
-          print(exc)
+        try:
+            yFile = yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
 
     sourceList = []
     LCFileList = []
@@ -71,35 +74,46 @@ def getSourcesFromYaml(yamlName, LCpath):
     decList = []
     lonList = []
     latList = []
-    print "found %i sources"%len(yFile)
-    for s in yFile.keys():
-       LCfile = "%s/%s/%s_lightcurve.fits"%(LCpath,s,s.lower())
-       print s
-       if os.path.exists(LCfile):
-          LCFileList.append(LCfile)
-       else:
-          print "LC file not found %s"%LCfile
-          continue
-       Locfile = "%s/%s/%s_loc.fits"%(LCpath,s,s.lower())
-       if os.path.exists(Locfile):
-          ra, dec, lon, lat = getPosition(Locfile)
-       else:
-          print "localization file not found %s"%Locfile
-          continue
-       raList.append(ra)
-       decList.append(dec)
-       lonList.append(lon)
-       latList.append(lat)
-       sourceList.append(s)
 
-    new_cols=[Column(name='Source_name',data=sourceList),Column(name='RAJ2000',data=raList),Column(name='DEJ2000',data=decList), Column(name='GLON',data=lonList), Column(name='GLAT',data=latList)]
+    keys = sorted(yFile.keys())
+    print "found %i sources" % len(yFile)
+    # for s in yFile.keys():
+    for s in keys:
+        LCfile = "%s/%s/%s_lightcurve.fits" % (LCpath, s, s.lower())
+        print s
+        if os.path.exists(LCfile):
+            LCFileList.append(LCfile)
+        else:
+            print "LC file not found %s" % LCfile
+            continue
+        Locfile = "%s/%s/%s_loc.fits" % (LCpath, s, s.lower())
+        if os.path.exists(Locfile):
+            ra, dec, lon, lat = getPosition(Locfile)
+        else:
+            print "localization file not found %s" % Locfile
+            continue
+        raList.append(ra)
+        decList.append(dec)
+        lonList.append(lon)
+        latList.append(lat)
+        sourceList.append(s)
+
+    new_cols = [Column(name='Source_Name', data=sourceList),
+                Column(name='RAJ2000', data=raList),
+                Column(name='DEJ2000', data=decList),
+                Column(name='GLON', data=lonList),
+                Column(name='GLAT', data=latList)]
     return new_cols, LCFileList
 
 
-yamlName = 'sourceListAll2283.yaml'
-LCpath = '/nfs/farm/g/glast/g/neutrino/shared/MonthlyLC1GeV/'
-new_cols, LCFileList = getSourcesFromYaml(yamlName, LCpath)
-tab = stack_tables(LCFileList, hdu=None, new_cols=new_cols)
-tab.write(yamlName.replace('.yaml','.fits'), format='fits')   
+def main():
+    yamlName = 'sourceListAll2283.yaml'
+    LCpath = '/nfs/farm/g/glast/g/neutrino/shared/MonthlyLC1GeV/'
+    new_cols, LCFileList = getSourcesFromYaml(yamlName, LCpath)
+    tab = stack_tables(LCFileList, hdu=None, new_cols=new_cols)
+    tab.write(yamlName.replace('.yaml', '.fits'),
+              format='fits', overwrite=True)
 
 
+if __name__ == "__main__":
+    main()
