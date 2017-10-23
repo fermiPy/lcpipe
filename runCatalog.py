4210f92be7205dbfebb12977d8ebe954e6fd9c67
afrancko <anna.franckowiak@gmail.com>
2017-10-23 13:39:20 -0700

    modified runCatalog.py

--- runCatalog.py
+++ runCatalog.py
@@ -56,6 +56,14 @@ def getPosition(fitsFile):
     return locData['ra'][0], locData['dec'][0], locData['glon'][0], locData['glat'][0]
 
 
+def getFlux(fitsFile, sourceName):
+    hdulist = fits.open(fitsFile)
+    catData = hdulist['catalog'].data
+    hdulist.close()
+    mask = catData['Source_Name'] == sourceName.replace('_',' ')
+    return catData[mask]['flux'][0], catData[mask]['flux_err'][0], catData[mask]['flux_ul95'][0], catData[mask]['eflux'][0], catData[mask]['eflux_err'][0], catData[mask]['eflux_ul95'][0]
+
+
 def getLC(CLFile):
     hdulist = fits.open(fitsFile)
     return hdulist['LIGHTCURVE'].data
@@ -74,6 +82,13 @@ def getSourcesFromYaml(yamlName, LCpath):
     decList = []
     lonList = []
     latList = []
+    fluxList = []
+    fluxErrList = []
+    efluxList = []
+    efluxErrList = []
+    fluxULList = []
+    efluxULList = []
+
 
     keys = sorted(yFile.keys())
     print "found %i sources" % len(yFile)
@@ -92,17 +107,36 @@ def getSourcesFromYaml(yamlName, LCpath):
         else:
             print "localization file not found %s" % Locfile
             continue
+        resFile = "%s/%s/fit0.fits" % (LCpath, s)
+        if os.path.exists(resFile):
+            flux, flux_err, fluxUL, eflux, eflux_err, efluxUL = getFlux(resFile, s)
+        else:
+            continue
+
         raList.append(ra)
         decList.append(dec)
         lonList.append(lon)
         latList.append(lat)
         sourceList.append(s)
+        fluxList.append(flux)
+        fluxErrList.append(flux_err)
+        efluxList.append(eflux)
+        efluxErrList.append(eflux_err)
+        fluxULList.append(fluxUL)
+        efluxULList.append(efluxUL)
+
 
     new_cols = [Column(name='Source_Name', data=sourceList),
                 Column(name='RAJ2000', data=raList),
                 Column(name='DEJ2000', data=decList),
                 Column(name='GLON', data=lonList),
-                Column(name='GLAT', data=latList)]
+                Column(name='GLAT', data=latList),
+                Column(name='Flux', data=fluxList),
+                Column(name='Energy_Flux', data=efluxList),
+                Column(name='Flux_Unc', data=fluxErrList),
+                Column(name='Energy_Flux_Unc', data=efluxErrList),
+                Column(name='Flux_UL95', data=fluxULList),
+                Column(name='Energy_Flux_UL95', data=efluxULList)]
     return new_cols, LCFileList
 
 
